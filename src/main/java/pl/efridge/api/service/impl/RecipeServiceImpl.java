588b3820bce3afc1df8347e9c7d122fc8f5f3437
bartek <gekarez@gmail.com>
2019-06-04 21:53:38 +0200

    Add more recipes

--- src/main/java/pl/efridge/api/service/impl/RecipeServiceImpl.java
+++ src/main/java/pl/efridge/api/service/impl/RecipeServiceImpl.java
@@ -12,33 +12,54 @@ import java.util.stream.Collectors;
 
 @Service
 public class RecipeServiceImpl implements RecipeService {
-    private List<Recipe> recipes = Arrays.asList(new Recipe(1, "Gulasz z parówek", "Przygotowanie:\n" +
+    private List<Recipe> recipes = Arrays.asList(new Recipe(1, "Gulasz z parówek",
                     "Parówki pokrój w plasterki.\n" +
-                    "Pieczarki, czosnek oraz cebulę obierz i pokrój w plasterki.\n" +
-                    "Na patelni rozgrzej olej i podsmaż na nim pieczarki, czosnek i cebulę.\n" +
-                    "Pomidory sparz, obierz ze skórki i posiekaj.\n" +
-                    "W garnuszki zagotuj 0,5 litra wody, dodaj kostkę rosołową, parówki i pomidory.\n" +
-                    "Całość duś kilka minut.\n" +
-                    "Przed podaniem posyp szczypiorkiem.", Arrays.asList(
+                            "Pieczarki, czosnek oraz cebulę obierz i pokrój w plasterki.\n" +
+                            "Na patelni rozgrzej olej i podsmaż na nim pieczarki, czosnek i cebulę.\n" +
+                            "Pomidory sparz, obierz ze skórki i posiekaj.\n" +
+                            "W garnuszki zagotuj 0,5 litra wody, dodaj kostkę rosołową, parówki i pomidory.\n" +
+                            "Całość duś kilka minut.\n" +
+                            "Przed podaniem posyp szczypiorkiem.", Arrays.asList(
             new Ingredient(1, "Parówka", 6, "sztuka"),
             new Ingredient(2, "Pieczarka", 100, "gram"),
             new Ingredient(3, "Cebula", 1, "sztuka"),
             new Ingredient(4, "Pomidor", 2, "sztuka"),
             new Ingredient(5, "Czosnek", 2, "ząbek")
             )),
-            new Recipe(2, "Ryż z parówkami", "Przygotowanie:\n" +
+            new Recipe(2, "Ryż z parówkami",
                     "Ryż ugotować na sypko.\n" +
-                    "Odsączyć i wymieszać z odrobiną oliwy z oliwek.\n" +
-                    "Cebulę pokroić w kostkę i zeszklić na patelni.\n" +
-                    "Parówki obrać i pokroić w talarki, paprykę pokroić w kostkę.\n" +
-                    "Wszystko dodać do cebuli i dusić ok. 10-15 minut.\n" +
-                    "Dodać ryż, wymieszać i doprawić do smaku.\n" +
-                    "Posypać posiekaną natką pietruszki.", Arrays.asList(
+                            "Odsączyć i wymieszać z odrobiną oliwy z oliwek.\n" +
+                            "Cebulę pokroić w kostkę i zeszklić na patelni.\n" +
+                            "Parówki obrać i pokroić w talarki, paprykę pokroić w kostkę.\n" +
+                            "Wszystko dodać do cebuli i dusić ok. 10-15 minut.\n" +
+                            "Dodać ryż, wymieszać i doprawić do smaku.\n" +
+                            "Posypać posiekaną natką pietruszki.", Arrays.asList(
                     new Ingredient(6, "Ryż", 2, "sztuka"),
                     new Ingredient(1, "Parówka", 6, "sztuka"),
                     new Ingredient(7, "Papryka", 2, "sztuka"),
                     new Ingredient(3, "Cebula", 2, "sztuka")
-            )));
+            )),
+            new Recipe(3, "Leczo z parówką, ogórkiem i papryką",
+                    "Przygotowujemy wszystkie składniki potrzebne do przygotowania leczo. Paprykę oczyszczamy z nasion i kroimy w paski. Ogórka obieramy i kroimy w plasterki. Pomidory natomiast na kawałki.\n" +
+                            "Parówki obieramy i kroimy w plasterki. Podsmażamy lekko razem z posiekaną cebulą. Wrzucamy paprykę oraz ogórka. Dusimy około 20 minut. Następnie wrzucamy pomidory i dusimy jeszcze chwilę. Doprawiamy do smaku pieprzem oraz solą.", Arrays.asList(
+                    new Ingredient(1, "Parówka", 3, "sztuka"),
+                    new Ingredient(8, "Ogórek", 1, "sztuka"),
+                    new Ingredient(7, "Papryka", 2, "sztuka"),
+                    new Ingredient(3, "Cebula", 1, "sztuka"),
+                    new Ingredient(4, "Pomidor", 2, "sztuka")
+            )),
+            new Recipe(4, "Makaron z kurczakiem i pomidorami",
+                    "Makaron ugotuj wg opisu na opakowaniu, a następnie odsącz.\n" +
+                            "Mięso pokrój w kostkę.\n" +
+                            "Cebulę i czosnek obierz i pokrój w kostkę, zeszklij na rozgrzanym maśle razem z mięsem, wkrusz kostkę. Całość podsmaż.\n" +
+                            "Dodaj pomidory, duś 10 minut, następnie wlej śmietankę i duś kolejne 10 minut.\n" +
+                            "Dopraw pieprzem i bazylią. Połącz całość z makaronem.\n" +
+                            "Całość udekoruj listkami świeżej bazylii.\n",
+                    Arrays.asList(
+                            new Ingredient(14, "Makaron", 200, "gram"),
+                            new Ingredient(12, "Filet", 6, "sztuka"),
+                            new Ingredient(13, "Sos", 1, "sztuka")
+                    )));
 
     @Override
     public List<Recipe> getAllRecipes() {
