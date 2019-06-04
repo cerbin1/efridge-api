a50af27baf7e31299a4978829f667c100535c3dd
Bartosz <gekarez@gmail.com>
2019-06-04 22:18:32 +0200

    Deploy

---
+++ docs/precache-manifest.29fedcbb5aa6606dfd0051cdcb6f4fca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1c2d972ea5e8c54695dc22ff31edc89",
+    "url": "/eFrigde/index.html"
+  },
+  {
+    "revision": "2840ae03d1caacc6d6cd",
+    "url": "/eFrigde/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "1356c831cba008fae97d",
+    "url": "/eFrigde/static/css/main.95832410.chunk.css"
+  },
+  {
+    "revision": "2840ae03d1caacc6d6cd",
+    "url": "/eFrigde/static/js/2.6ae5e948.chunk.js"
+  },
+  {
+    "revision": "1356c831cba008fae97d",
+    "url": "/eFrigde/static/js/main.9c03ce40.chunk.js"
+  },
+  {
+    "revision": "0d36ddc317e0021c6027",
+    "url": "/eFrigde/static/js/runtime~main.65e9ec89.js"
+  }
+]);
